d7aff09acc57bf1d5f7d583ec8a2b06a95fb91b0
8ctavio89 <60673282+8ctavio89@users.noreply.github.com>
2021-11-16 19:53:30 -0700

    Cambio en JavaScript.

--- js_scripts/login_validation.js
+++ js_scripts/login_validation.js
@@ -3,36 +3,32 @@
 var username = document.forms["login_form"]["username"];
 var password = document.forms["login_form"]["password"];
 
-var username_validation_error = document.getElementById(
-  "username_validation_error"
-);
-var password_validation_error = document.getElementById(
-  "password_validation_error"
-);
+var u_validation_error = document.getElementById("u_validation_error");
+var p_validation_error = document.getElementById("p_validation_error");
 
 var form_validation_error = document.getElementById("form_validation_error");
 
 function validateLogInInfo() {
   if (username.value.length < 1 || username.value.length > 50) {
-    username_validation_error.style.border = "1px solid red";
-    username_validation_error.innerHTML = "Formato de entrada inválido.";
-    username_validation_error.style.display = "block";
+    u_validation_error.style.border = "1px solid red";
+    u_validation_error.innerHTML = "Formato de entrada inválido.";
+    u_validation_error.style.display = "block";
 
     return false;
   } else {
-    username_validation_error.style.display = "none";
+    u_validation_error.style.display = "none";
     username.style.border = "1px solid #4caf50";
   }
 
   if (password.value.length < 1 || username.value.length > 100) {
-    password_validation_error.style.border = "1px solid red";
-    username_validation_error.innerHTML =
+    p_validation_error.style.border = "1px solid red";
+    u_validation_error.innerHTML =
       "Formato de contraseña incorrecto. Intenta de nuevo.";
-    password_validation_error.style.display = "block";
+    p_validation_error.style.display = "block";
 
     return false;
   } else {
-    password_validation_error.style.display = "none";
+    p_validation_error.style.display = "none";
     password.style.border = "1px solid #4caf50";
   }
 }
